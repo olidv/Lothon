14c617e0929abd5086389440e797b7560a5ff638
Oliveira Brasil <ramo.ltda@gmail.com>
2022-02-21 08:45:36 -0300

    Novos scripts para atualizacao do projeto e aplicacao.

---
+++ src/scripts/upgrade.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo Posicionando no diretorio raiz da aplicacao
+cd ..
+echo.
+
+echo Atualizando o pip no sistema para evitar conflitos de versoes
+python -m pip install --upgrade pip
+echo.
+
+echo Verificando a versao atual do pip no sistema
+pip --version
+echo.
+
+echo Ativa o ambiente virtual da aplicacao
+call venv\Scripts\activate.bat
+echo.
+
+echo Atualizando o pip no ambiente para evitar conflitos de versoes
+python -m pip install --upgrade pip
+echo.
+
+echo Verificando a versao atual do pip no ambiente
+pip --version
+echo.
+
+echo Instalando as dependencias da aplicacao no ambiente
+python -m pip install -r requirements.txt
+echo.
+
+rem Pausa final...
+echo.
+pause
