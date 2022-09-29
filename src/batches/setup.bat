cc160039702afa128b367d3c6e03d3569cc3ef3d
Raul Bras <br.eve.author@gmail.com>
2022-09-29 20:39:37 -0300

    Refactoring para executar a plataforma Infinite no note Dell.

--- src/batches/setup.bat
+++ src/batches/setup.bat
@@ -25,6 +25,18 @@ echo Ativando o ambiente virtual da aplicacao
 call venv\Scripts\activate.bat
 echo.
 
+echo Verificando a versao atual do Python no ambiente
+python --version
+echo.
+
+echo Verificando a versao atual do pip no ambiente
+pip --version
+echo.
+
+echo Atualizando o pip no ambiente para evitar conflitos de versoes
+python -m pip install --upgrade pip
+echo.
+
 echo Instalando as dependencias da aplicacao no ambiente
 python -m pip install -r requirements.txt
 echo.
