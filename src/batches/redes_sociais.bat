70f8a67fcc3469c8b9ad8870cff2f40fc13b516d
Raul Bras <br.eve.author@gmail.com>
2022-10-03 12:06:43 -0300

    Novos scripts batch.

---
+++ src/batches/redes_sociais.bat
@@ -0,0 +1,6 @@
+@echo off
+color 1F
+
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url https://web.whatsapp.com https://web.telegram.org/z/#-1488280660 https://www.instagram.com/loto365br https://www.facebook.com/Loto365br https://www.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos https://m.kwai.com/user/150001283084907 https://twitter.com/Loto365br https://www.linkedin.com/company/85606564/admin/notifications/all https://br.pinterest.com/loto365br
+
+exit
