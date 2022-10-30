d07fe3a51c54ea88fe58c67af3ad8af630026392
Raul Bras <br.eve.author@gmail.com>
2022-10-30 13:09:53 -0300

    Melhoria dos processos.

--- src/batches/novos_palpites.bat
+++ src/batches/novos_palpites.bat
@@ -4,6 +4,7 @@ color 1F
 cd /D C:\Users\qdev\Loto365\docs-templates\Social
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "C:\Users\qdev\Loto365\docs-templates\Social\descricao.txt"
 
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660" "https://twitter.com/compose/tweet" "https://www.facebook.com/Loto365br" "https://www.instagram.com/loto365br/" "https://www.linkedin.com/company/loto365br/?share=true" "https://studio.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos/upload" "https://br.pinterest.com/pin-builder/"
+rem start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660" "https://twitter.com/compose/tweet" "https://www.facebook.com/Loto365br" "https://www.instagram.com/loto365br/" "https://www.linkedin.com/company/loto365br/?share=true" "https://studio.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos/upload" "https://br.pinterest.com/pin-builder/"
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://www.loto365.com.br/#palpites" "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660" "https://studio.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos/upload"
 
 exit
