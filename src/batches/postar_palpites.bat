6dcd5a51187d4df69781285eae0774d41a68e913
Raul Bras <br.eve.author@gmail.com>
2022-10-31 19:09:55 -0300

    Nao teremos videos por enquanto.

--- src/batches/postar_palpites.bat
+++ src/batches/postar_palpites.bat
@@ -39,7 +39,7 @@ echo Executando Firefox para verificar Loto365.com.br e atualizar Google Drive e
 cd /D C:\Users\qdev\Loto365\docs-templates\Social
 start "" "C:\Program Files (x86)\Notepad++\notepad++.exe" "C:\Users\qdev\Loto365\docs-templates\Social\descricao.txt" "C:\Users\qdev\Loto365\cdn-lothon\data\palpites\dia-de-sorte.csv"
 rem start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://www.Loto365.com.br/#palpites" "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660" "https://twitter.com/compose/tweet" "https://www.facebook.com/Loto365br" "https://www.instagram.com/loto365br/" "https://www.linkedin.com/company/loto365br/?share=true" "https://studio.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos/upload" "https://br.pinterest.com/pin-builder/"
-start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://www.Loto365.com.br/#palpites" "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660" "https://studio.youtube.com/channel/UCiiGBkWJiej2eAfwcqiI77A/videos/upload"
+start "" "C:\Program Files\Mozilla Firefox\firefox.exe" -url "https://www.Loto365.com.br/#palpites" "https://drive.google.com/drive/folders/1PjOJoHbueNMiNGKJHLP5tC7UEyuRRSvD" "https://web.whatsapp.com/" "https://web.telegram.org/z/#-1488280660"
 start .
 echo.
 
